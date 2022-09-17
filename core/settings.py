5db86b4533a3530e2493e51918eeee4ff3200ebb
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-09-17 21:12:08 +0545

    Create custom user model and add it to admin page

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+
+    'accounts.apps.AccountsConfig',
+    'todo.apps.TodoConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
