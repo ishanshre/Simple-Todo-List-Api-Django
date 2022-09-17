bc37e97878a8798bf12600640222f3658d46b675
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-09-17 21:23:59 +0545

    create todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    completed = models.BooleanField(default=False)
+
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
